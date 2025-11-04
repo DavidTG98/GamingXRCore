b0b02992944805fcf1181bbc3bacac18442655ca
DavidThiagoGarcia <david.gasparin@compasso.com.br>
2025-11-04 13:44:54 -0300

    (chore): re-add auto increment version

---
+++ Utils/Editor/AutoVersionIncrement.cs
@@ -0,0 +1,46 @@
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+namespace GamingXRCore.Utils
+{
+    internal class AutoVersionIncrement : IPreprocessBuildWithReport
+    {
+        public int callbackOrder => 0;
+
+        public void OnPreprocessBuild(BuildReport report)
+        {
+#if UNITY_SERVER
+            return;
+#endif
+
+            ImproveVersion();
+        }
+
+        [MenuItem("GamingXRCore/Increment Version")]
+        private static void ImproveVersion()
+        {
+            string currentVersion = PlayerSettings.bundleVersion;
+
+            // Divide por pontos
+            string[] parts = currentVersion.Split('.');
+            int major = int.Parse(parts[0]);
+            int minor = int.Parse(parts[1]);
+            int patch = int.Parse(parts[2]);
+
+            // Incrementa o patch
+            patch++;
+
+            // Atualiza a versão
+            string newVersion = $"{major}.{minor}.{patch}";
+            PlayerSettings.bundleVersion = newVersion;
+
+            // Salva no projeto
+            AssetDatabase.SaveAssets();
+            Debug.Log($"Versão incrementada automaticamente: {currentVersion} -> {newVersion}");
+        }
+    }
+}
+#endif
